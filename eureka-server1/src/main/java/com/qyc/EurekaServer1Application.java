1a88cb50dc92c512dd3eab6e058f6cad571dec78
qyc <qyc772528883@163.com>
2019-11-10 16:48:15 +0800

    second commit spring-cloud-demo

--- eureka-server1/src/main/java/com/qyc/EurekaServer1Application.java
+++ eureka-server1/src/main/java/com/qyc/EurekaServer1Application.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServer1Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer1Application.class, args);
     }
